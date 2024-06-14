23a80ebeb424a441a3ec444ec133b40826d2130c
Leo Wilson <leo@lwilson.dev>
2024-06-14 10:37:58 -0700

    Catppuccin Mocha

---
+++ src/catppuccin.rs
@@ -0,0 +1,32 @@
+// turn off unused constant warning
+// as it is a color palette and not all colors are used
+#![allow(dead_code)]
+
+use crate::colorize::RGB;
+
+pub const MOCHA_ROSEWATER: RGB = RGB(245, 224, 220);
+pub const MOCHA_FLAMINGO: RGB  = RGB(242, 205, 205);
+pub const MOCHA_PINK: RGB      = RGB(245, 194, 231);
+pub const MOCHA_MAUVE: RGB     = RGB(203, 166, 247);
+pub const MOCHA_RED: RGB       = RGB(243, 139, 168);
+pub const MOCHA_MAROON: RGB    = RGB(235, 160, 172);
+pub const MOCHA_PEACH: RGB     = RGB(250, 179, 135);
+pub const MOCHA_YELLOW: RGB    = RGB(249, 226, 175);
+pub const MOCHA_GREEN: RGB     = RGB(166, 227, 161);
+pub const MOCHA_TEAL: RGB      = RGB(148, 226, 213);
+pub const MOCHA_SKY: RGB       = RGB(137, 220, 235);
+pub const MOCHA_SAPPHIRE: RGB  = RGB(116, 199, 236);
+pub const MOCHA_BLUE: RGB      = RGB(137, 180, 250);
+pub const MOCHA_LAVENDER: RGB  = RGB(180, 190, 254);
+pub const MOCHA_TEXT: RGB      = RGB(205, 214, 244);
+pub const MOCHA_SUBTEXT1: RGB  = RGB(186, 194, 222);
+pub const MOCHA_SUBTEXT0: RGB  = RGB(166, 173, 200);
+pub const MOCHA_OVERLAY2: RGB  = RGB(147, 153, 178);
+pub const MOCHA_OVERLAY1: RGB  = RGB(127, 132, 156);
+pub const MOCHA_OVERLAY0: RGB  = RGB(108, 112, 134);
+pub const MOCHA_SURFACE2: RGB  = RGB(88, 91, 112);
+pub const MOCHA_SURFACE1: RGB  = RGB(69, 71, 90);
+pub const MOCHA_SURFACE0: RGB  = RGB(49, 50, 68);
+pub const MOCHA_BASE: RGB      = RGB(30, 30, 46);
+pub const MOCHA_MANTLE: RGB    = RGB(24, 24, 37);
+pub const MOCHA_CRUST: RGB     = RGB(17, 17, 27);
